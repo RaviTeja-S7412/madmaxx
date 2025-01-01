590c0753b431d35c860ead318f926b468e942bc8
RaviTeja-S7412 <sakaraviteja.s@gmail.com>
2025-01-01 11:49:01 +0530

    updated encryption

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -1,118 +1,111 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
-	public function __construct(){
-			
+	public function __construct()
+	{
+
 		parent::__construct();
 		// if($this->session->userdata("admin_id")){
 		// 	redirect("dashboard");
 		// }
-		
+
 	}
 
-	public function index(){
+	public function index()
+	{
 
 		$this->load->view("admin/login");
 	}
 
 
-	function do_login(){
+	function do_login()
+	{
 
-		$email = $this->input->post("email",true);
-		$pass = $this->input->post("pass",true);
+		$email = $this->input->post("email", true);
+		$pass = $this->input->post("pass", true);
 
-		if(empty($email)){
-			$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>Please Enter User Name</div>';	
-			$this->session->set_flashdata("fmsg",$msg);
-			redirect("admin-login");
-
-		}
-		if(empty($pass)){
-			$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>Please Enter Password</div>';	
-			$this->session->set_flashdata("fmsg",$msg);
+		if (empty($email)) {
+			$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>Please Enter User Name</div>';
+			$this->session->set_flashdata("fmsg", $msg);
 			redirect("admin-login");
 		}
-		
-		$a = $this->db->get_where("tbl_auths",array("email"=>$email,"deleted"=>0,"status"=>"Active"))->row();
-	
-	
-		if($a){
+		if (empty($pass)) {
+			$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>Please Enter Password</div>';
+			$this->session->set_flashdata("fmsg", $msg);
+			redirect("admin-login");
+		}
 
-			$chk1 = $this->db->get_where("tbl_auths",array("email"=>$email,"deleted"=>0,"status"=>"Active"))->num_rows();
-			if($chk1==1){
+		$a = $this->db->get_where("tbl_auths", array("email" => $email, "deleted" => 0, "status" => "Active"))->row();
 
-			$u = $this->db->get_where("tbl_auths",array("email"=>$email,"deleted"=>0,"status"=>"Active"))->row();
-			$cpassword = $this->secure->decrypt($u->password);
 
-			if($pass==$cpassword){
-				
-				$date = date("Y-m-d H:i:s");
-				$data = array("last_logged_in"=>$date);
+		if ($a) {
 
-				$this->db->set($data);
-				$this->db->where("id",$u->id);
-				$this->db->update("tbl_auths");
+			$chk1 = $this->db->get_where("tbl_auths", array("email" => $email, "deleted" => 0, "status" => "Active"))->num_rows();
+			if ($chk1 == 1) {
 
-				$this->session->set_userdata(array("admin_id"=>$u->id,"email"=>$u->email));
+				$u = $this->db->get_where("tbl_auths", array("email" => $email, "deleted" => 0, "status" => "Active"))->row();
+				$cpassword = $this->encryption->decrypt($u->password);
 
-				$loginData = $this->agent();
-				$dd = $this->db->insert("tbl_login_history",$loginData);
+				if ($pass == $cpassword) {
 
-				redirect("admin/dashboard");
-					
-			}else{
+					$date = date("Y-m-d H:i:s");
+					$data = array("last_logged_in" => $date);
 
-				$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>Please Enter Correct Password</div>';	
-				$this->session->set_flashdata("fmsg",$msg);
-				redirect("admin-login");	
+					$this->db->set($data);
+					$this->db->where("id", $u->id);
+					$this->db->update("tbl_auths");
+
+					$this->session->set_userdata(array("admin_id" => $u->id, "email" => $u->email));
+
+					$loginData = $this->agent();
+					$dd = $this->db->insert("tbl_login_history", $loginData);
+
+					redirect("admin/dashboard");
+				} else {
+
+					$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>Please Enter Correct Password</div>';
+					$this->session->set_flashdata("fmsg", $msg);
+					redirect("admin-login");
+				}
+			} else {
+
+				$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>User Not Registered With Us Or Please Contact Administrator</div>';
+				$this->session->set_flashdata("fmsg", $msg);
+				redirect("admin-login");
 			}
-		
-		}else{
-
-			$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>User Not Registered With Us Or Please Contact Administrator</div>';	
-			$this->session->set_flashdata("fmsg",$msg);
+		} else {
+			$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>User Not Registered With Us Or Please Contact Administrator</div>';
+			$this->session->set_flashdata("fmsg", $msg);
 			redirect("admin-login");
 		}
-	
-	}else{
-		$msg = '<div class="alert alert-danger alert-dismissable"><button type = "button" class ="close" data-dismiss = "alert" aria-hidden = "true">&times;</button>User Not Registered With Us Or Please Contact Administrator</div>';	
-		$this->session->set_flashdata("fmsg",$msg);
-		redirect("admin-login");
-	}	
-}
+	}
 
 
 
-public function agent(){
-	if ($this->agent->is_browser())
-		{
-				$data['agent'] = $this->agent->browser().' '.$this->agent->version();
-		}
-		elseif ($this->agent->is_robot())
-		{
-			   $data['agent'] = $this->agent->robot();
-		}
-		elseif ($this->agent->is_mobile())
-		{
-			   $data['agent'] = $this->agent->mobile();
-		}
-		else
-		{
-			   $data['agent'] = 'Unidentified User Agent';
+	public function agent()
+	{
+		if ($this->agent->is_browser()) {
+			$data['agent'] = $this->agent->browser() . ' ' . $this->agent->version();
+		} elseif ($this->agent->is_robot()) {
+			$data['agent'] = $this->agent->robot();
+		} elseif ($this->agent->is_mobile()) {
+			$data['agent'] = $this->agent->mobile();
+		} else {
+			$data['agent'] = 'Unidentified User Agent';
 		}
 
-		$data['platform']= $this->agent->platform(); 
+		$data['platform'] = $this->agent->platform();
 		$data['ip_address'] = $this->input->ip_address();
-		
+
 		return $data;
-}
-public function logout(){
-	
-	$this->session->sess_destroy();
-	redirect("admin-login");
-}
+	}
+	public function logout()
+	{
 
-
+		$this->session->sess_destroy();
+		redirect("admin-login");
+	}
 }
--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -11,7 +11,7 @@
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<? echo base_url() ?>assets/proxima.png">
     <title>Proxima</title>
-	<link rel="canonical" href="https://www.wrappixel.com/templates/monsteradmin/" />
+    <link rel="canonical" href="https://www.wrappixel.com/templates/monsteradmin/" />
     <!-- Custom CSS -->
     <link href="<? echo base_url() ?>assets/dist/css/style.min.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -39,23 +39,30 @@
         <!-- ============================================================== -->
         <!-- Login box.scss -->
         <!-- ============================================================== -->
-        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background:url(<? echo base_url() ?>assets/images/background/login-register.jpg) no-repeat center center; background-size: cover;">
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center"
+            style="background:url(<? echo base_url() ?>assets/images/background/login-register.jpg) no-repeat center center; background-size: cover;">
             <div class="auth-box p-4 bg-white rounded">
                 <div id="loginform">
                     <div class="logo">
                         <h3 class="box-title mb-3">Sign In</h3>
                     </div>
+                    <? echo $this->session->flashdata("fmsg") ?>
                     <!-- Form -->
                     <div class="row">
                         <div class="col-12">
-                            <form class="form-horizontal mt-3 form-material" method="post" action="<? echo base_url('admin/login/do_login') ?>">
+                            <form class="form-horizontal mt-3 form-material" method="post"
+                                action="<? echo base_url('admin/login/do_login') ?>">
                                 <div class="form-group mb-3">
                                     <div class="">
-                                        <input class="form-control" type="text" name="email" required="" placeholder="Username"> </div>
+                                        <input class="form-control" type="text" name="email" required=""
+                                            placeholder="Username">
+                                    </div>
                                 </div>
                                 <div class="form-group mb-4">
                                     <div class="">
-                                        <input class="form-control" type="password" name="pass" required="" placeholder="Password"> </div>
+                                        <input class="form-control" type="password" name="pass" required=""
+                                            placeholder="Password">
+                                    </div>
                                 </div>
                                 <!--<div class="form-group">
                                     <div class="d-flex">
@@ -66,15 +73,17 @@
                                 </div>-->
                                 <div class="form-group text-center mt-4">
                                     <div class="col-xs-12">
-                                        <button class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light" type="submit">Log In</button>
+                                        <button
+                                            class="btn btn-info btn-lg btn-block text-uppercase waves-effect waves-light"
+                                            type="submit">Log In</button>
                                     </div>
                                 </div>
-                                
+
                             </form>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </div>
         <!-- ============================================================== -->
